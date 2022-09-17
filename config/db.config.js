69b763b0626ddea219259cb830a32eee7944db49
jalun118 <jalunugroho118@gmail.com>
2022-09-17 14:31:50 +0700

    update database url

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: "mongodb+srv://jalunugroho118:jalunugroho010108@percobaan1.vkw3bbs.mongodb.net/?retryWrites=true&w=majority"
+  url: "mongodb+srv://jalunugroho118:jalunugroho010108@db-kenangan.jf11a8w.mongodb.net/kenangan-db?retryWrites=true&w=majority"
 }
