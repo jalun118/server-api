15a81a7e125866c82bbc63685e2efe6db3858f1f
jalun118 <jalunugroho118@gmail.com>
2022-10-08 20:27:51 +0700

    menambahkan validation url

--- app/Validation/Validation.js
+++ app/Validation/Validation.js
@@ -6,6 +6,8 @@ exports.runValidation = (req, res, next) => {
   if (!error.isEmpty()) {
     return res.status(409).json({
       status: false,
+      response: 409,
+      fieldError: error.array()[0].param,
       message: error.array()[0].msg
     });
   }
@@ -14,16 +16,18 @@ exports.runValidation = (req, res, next) => {
 
 exports.validationTeman = [
   check('no_absen', 'No absen harus diisi').notEmpty().isNumeric().withMessage('No absen harus angka'),
-  check('kelas', 'Nama harus diisi').notEmpty(),
+  check('nama', 'Nama harus diisi').notEmpty(),
 ]
 
+const RegExUrl = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_.~#?&//=]*)/g;
+
 exports.validationFoto = [
-  check('link_original_foto', 'Link Foto harus diisi').notEmpty(),
+  check('link_original_foto', 'Link Foto harus diisi').notEmpty().matches(RegExUrl).withMessage("bukan link"),
   check('judul', 'Judul harus diisi').notEmpty(),
 ]
 
 exports.validationVideo = [
-  check('link_original_video', 'video Foto harus diisi').notEmpty(),
+  check('link_original_video', 'video Foto harus diisi').notEmpty().matches(RegExUrl).withMessage("bukan link"),
   check('judul', 'Judul harus diisi').notEmpty(),
 ]
 
