0c83b17faa3bd97d26bca8dbc881c8d9815d55e5
jalun118 <jalunugroho118@gmail.com>
2022-10-08 20:31:20 +0700

    membuat url demo

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
+  // url: "mongodb://127.0.0.1:27017/db_kenangan" //demo
   url: "mongodb+srv://jalunugroho118:jalunugroho010108@db-kenangan.jf11a8w.mongodb.net/kenangan-db?retryWrites=true&w=majority"
 }
